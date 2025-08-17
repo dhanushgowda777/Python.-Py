c8e0a2735922675f9687c7c3013f925c417d4aa3
dhanushgowda777 <dhanushgowda541@gmail.com>
2025-08-17 20:15:10 +0530

    Add files via upload
    My first python project of arithmetic operators..

---
+++ dhanush 20.py
@@ -0,0 +1,10 @@
+a = 15
+b = 4
+
+print("Addition (a + b) =", a + b)
+print("Subtraction (a - b) =", a - b)
+print("Multiplication (a * b) =", a * b)
+print("Division (a / b) =", a / b)
+print("Modulus (a % b) =", a % b)
+print("Exponentiation (a ** b) =", a ** b)
+print("Floor Division (a // b) =", a // b)
