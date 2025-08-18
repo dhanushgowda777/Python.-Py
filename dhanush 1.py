24a0308bdbe21bd704a031f53115a97b3a459a07
dhanushgowda777 <dhanushgowda541@gmail.com>
2025-08-18 07:56:38 +0530

    Add files via upload
    Today i did some program of python, input output operaters and concatenation
    
    #Python #Coding #100days

---
+++ dhanush 1.py
@@ -0,0 +1,7 @@
+boy_name = input("Boy name: ")
+boy_age = int(input("Boy age: "))
+girl_name = input("Girl name: ")
+girl_age = int(input("Girl age: "))
+age_diff = boy_age - girl_age
+print(boy_name + " loves " + girl_name + ". Age difference is " + str(age_diff))
+print(f"{boy_name} loves {girl_name}. Age difference is {age_diff}")        
