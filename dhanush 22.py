2772c2d29ed656818eacd49f2128786029a10d37
dhanushgowda777 <dhanushgowda541@gmail.com>
2025-08-18 21:21:04 +0530

    Add files via upload
    Today i learnt some python concepts of string methods.
    #python #100dayofcode #code

---
+++ dhanush 22.py
@@ -0,0 +1,8 @@
+# Strings
+text = "hii srujan"
+print("Original:", text)
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("First 5 characters:", text[:5])
+print("Length:", len(text))
+print("Replace:", text.replace("srujan", "dhanush"))
