e7b2483920570e64fc4194a01ff403f98d62429c
Dhanush <dhanushgowda541@gmail.com>
2025-08-22 22:59:10 +0530

    Add files via upload
    Today learned dictionary concept of python

---
+++ dhanush25.py
@@ -0,0 +1,12 @@
+# Dictionaries (key-value pairs)
+student = {
+    "name": "dhanush",
+    "age": 20,
+    "course": "Python"
+}
+print("Dictionary:", student)
+
+print("Name:", student["name"])
+student["age"] = 18   # Update
+student["city"] = "bengaluru"  # Add
+print("Updated Dictionary:", student)
